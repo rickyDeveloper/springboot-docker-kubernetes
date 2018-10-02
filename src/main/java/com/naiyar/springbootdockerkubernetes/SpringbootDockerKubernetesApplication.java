de547475d7d98a19868a4aae2358d7e294d1a7ef
“Vikas <naiyarv@gmail.com>
2018-10-02 09:09:18 +0530

    Added node docker image

---
+++ src/main/java/com/naiyar/springbootdockerkubernetes/SpringbootDockerKubernetesApplication.java
@@ -0,0 +1,12 @@
+package com.naiyar.springbootdockerkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerKubernetesApplication.class, args);
+	}
+}
